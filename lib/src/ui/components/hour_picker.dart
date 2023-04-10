eb2db6fa2b3507cc237a5f2196af21ddf55c80d7
BenjaminAlvarezDiaz <benja77744@gmail.com>
2023-04-09 21:05:50 -0300

    Haciendo un hour picker con ruleta

--- lib/src/ui/components/hour_picker.dart
+++ lib/src/ui/components/hour_picker.dart
@@ -22,8 +22,8 @@ class HourPicker extends StatefulWidget {
 }
 
 class _HourPickerState extends State<HourPicker> {
-  late int _hour;
-  late int _minute;
+  int _hour = DateTime.now().hour;
+  int _minute = DateTime.now().minute;
   late TimeOfDay _selectedTime;
 
   @override
@@ -50,30 +50,41 @@ class _HourPickerState extends State<HourPicker> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.red,
-      height: 200,
-      width: 400,
-      child: Row(
-        children: [
-          Container(
-            height: 200,
-            child: Text('${_selectedTime.hour}', style: TextStyle(fontSize: widget.fontSize),),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                  child: Text(':', style: TextStyle(fontSize: widget.fontSize),)),
-              Container(height: 50, color: Colors.white,)
-            ],
-          ),
-          Container(
-            height: 200,
-            child: Text('${_selectedTime.minute}', style: TextStyle(fontSize: widget.fontSize),),
-          ),
-        ],
-      )
+    return InkWell(
+      onTap: (){
+
+      },
+      child: Container(
+        color: Colors.white,
+        height: 200,
+        width: 400,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _buildWheelPicker(
+              'Hour',
+              List.generate(24, (i) => i),
+                  (index) {
+                setState(() {
+                  _hour = index;
+                });
+              },
+              _hour,
+            ),
+            SizedBox(width: 20),
+            _buildWheelPicker(
+              'Minute',
+              List.generate(60, (i) => i),
+                  (index) {
+                setState(() {
+                  _minute = index;
+                });
+              },
+              _minute,
+            ),
+          ],
+        ),
+      ),
     );
   }
 
@@ -94,6 +105,51 @@ class _HourPickerState extends State<HourPicker> {
           ),
         ),
         child: Center(child: Container(height: 100, width: 100, color: Colors.white,))
+    );
+  }
+
+  Widget _buildWheelPicker(
+      String title, List<int> items, Function(int) onChanged, int initialValue) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          title,
+          style: TextStyle(fontSize: 20),
+        ),
+        SizedBox(height: 10),
+        Container(
+          height: 100,
+          width: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(20),
+            color: Colors.grey[200],
+          ),
+          child: ListWheelScrollView(
+            //controller: ScrollController(initialScrollOffset: initialValue as double),
+            itemExtent: 40,
+            children: items.map((item) {
+              return Center(
+                child: Text(
+                  item.toString(),
+                  style: TextStyle(fontSize: 24),
+                ),
+              );
+            }).toList(),
+            onSelectedItemChanged: onChanged,
+            physics: FixedExtentScrollPhysics(),
+            useMagnifier: true,
+            magnification: 1.2,
+            squeeze: 1.4,
+            diameterRatio: 1.3,
+            perspective: 0.003,
+            offAxisFraction: 0,
+            clipBehavior: Clip.none,
+            overAndUnderCenterOpacity: 0.7,
+            renderChildrenOutsideViewport: true,
+          ),
+        ),
+      ],
     );
   }
 }
