b1d2dfae5bc09b31fd757b3516dd059738b14a62
BenjaminAlvarezDiaz <benja77744@gmail.com>
2023-10-25 00:36:10 -0300

    Agregando algunas dependencias y trabajando en la pantalla
    Se mejoro el espacio entre los botones de la pantalla

--- lib/src/ui/screens/choose_day_and_time_screen.dart
+++ lib/src/ui/screens/choose_day_and_time_screen.dart
@@ -66,199 +66,224 @@ class _ChooseDayAndTimeScreenState extends StateMVC<ChooseDayAndTimeScreen> {
 
   Widget body() {
     DateTime initialDate = DateTime.now();
-    return SingleChildScrollView(
-      child: Padding(
-        padding: EdgeInsets.all(20),
-        child: Container(
-          height: 800,
-          width: 600,
-          child: Stack(
-            children: [
-              //Positioned(top: 150, child: Container(height: 100, width: 200 ,color: KRed,)),
-              //eventDayFromAndTo(250, isToVisible, KSecondary, context),
-              /*Positioned(
-                  top: 90,
-                  left: 160,
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Text('Hasta:', style: TextStyle(fontSize: 16, color: KGrey2),),
-                          SizedBox(width: 100,)
-                        ],
-                      ),
-                      SizedBox(height: 2,),
-                      button(
-                          text: '16:00',
-                          fontSize: 16,
-                          iconLeft: Icons.calendar_today,
-                          iconRight: Icons.keyboard_arrow_down,
-                          function: (){
-                            setState(() {
-                              isToVisible = !isToVisible;
-                            });
-                          },
-                          height: 40,
-                          width: 140,
-                          content: Container()
-                      ),
-                    ],
-                  )
-              ),*/
-              isToVisible? Positioned(
-                top: 140,
-                left: 10,
-                child: Container(
-                  width: 300,
-                  height: 400,
-                  color: KWhite,
-                  child: Center(
-                    child: Text('Widget Superpuesto'),
-                  ),
-                ),
-              ) : Container(),
-              isFromVisible? Positioned(
-                top: 140,
-                left: 10,
-                child: Container(
-                  width: 300,
-                  height: 400,
-                  color: KRed,
-                  child: Center(
-                    child: Text('Widget Superpuesto'),
-                  ),
-                ),
-              ) : Container(),
-              //eventDayFromAndTo(150, isFromVisible, KBlack, context),
-              Positioned(
-                  top: 90,
-                  left: 10,
-                  child: Row(
-                    children: [
-                      Column(
-                        children: [
-                          Row(
-                            children: [
-                              Text('Desde:', style: TextStyle(fontSize: 16, color: KGrey2),),
-                              SizedBox(width: 90,)
-                            ],
-                          ),
-                          SizedBox(height: 2,),
-                          button(
-                              iconRightFlex: 1,
-                              iconLeftFlex: 1,
-                              textFlex: 2,
-                              contentFlex: 0,
-                              text: '16:00',
-                              fontSize: 16,
-                              iconLeft: Icons.access_time,
-                              iconRight: Icons.keyboard_arrow_down,
-                              function: (){
-                                setState(() {
-                                  isFromVisible = !isFromVisible;
-                                });
-                              },
-                              height: 40,
-                              width: 140,
-                              content: Container()
-                          ),
-                        ],
-                      ),
-                      SizedBox(width: 20,),
-                      Column(
-                        children: [
-                          Row(
-                            children: [
-                              Text('Hasta:', style: TextStyle(fontSize: 16, color: KGrey2),),
-                              SizedBox(width: 90,)
-                            ],
-                          ),
-                          SizedBox(height: 2,),
-                          button(
-                              iconRightFlex: 1,
-                              iconLeftFlex: 1,
-                              textFlex: 2,
-                              contentFlex: 0,
-                              text: '20:00',
-                              fontSize: 16,
-                              iconLeft: Icons.access_time,
-                              iconRight: Icons.keyboard_arrow_down,
-                              function: (){
-                                setState(() {
-                                  isToVisible = !isToVisible;
-                                });
-                              },
-                              height: 40,
-                              width: 140,
-                              content: Container()
-                          ),
-                        ],
-                      ),
+    return Center(
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Container(
+            height: 800,
+            width: double.infinity,
+            child: Stack(
+              children: [
+                //Positioned(top: 150, child: Container(height: 100, width: 200 ,color: KRed,)),
+                //eventDayFromAndTo(250, isToVisible, KSecondary, context),
+                /*Positioned(
+                    top: 90,
+                    left: 160,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Text('Hasta:', style: TextStyle(fontSize: 16, color: KGrey2),),
+                            SizedBox(width: 100,)
+                          ],
+                        ),
+                        SizedBox(height: 2,),
+                        button(
+                            text: '16:00',
+                            fontSize: 16,
+                            iconLeft: Icons.calendar_today,
+                            iconRight: Icons.keyboard_arrow_down,
+                            function: (){
+                              setState(() {
+                                isToVisible = !isToVisible;
+                              });
+                            },
+                            height: 40,
+                            width: 140,
+                            content: Container()
+                        ),
+                      ],
+                    )
+                ),*/
+                //eventDayFromAndTo(150, isFromVisible, KBlack, context),
+                //eventDayFromAndTo(50, isEventDayVisible, KRed, context),
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(width: 10,),
+                        Text('Elija día del evento:', style: TextStyle(fontSize: 20, color: KGrey2),),
+                        SizedBox(width: 140,)
+                      ],
+                    ),
+                    //Text('Elija día del evento:', style: TextStyle(fontSize: 20, color: KGrey2),),
+                    SizedBox(height: 5,),
+                    button(
+                        iconLeftFlex: 1,
+                        iconRightFlex: 1,
+                        textFlex: 6,
+                        contentFlex: 3,
+                        text: ' 09/08/2023',
+                        fontSize: 16,
+                        iconLeft: Icons.calendar_today,
+                        iconRight: Icons.keyboard_arrow_down,
+                            function: (){
+                          setState(() {
+                            isEventDayVisible = !isEventDayVisible;
+                          });
+                        },
+                      height: 40,
+                      width: 300,
+                      content: Container()
+                    ),
+                    SizedBox(height: 10,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text('Desde:', style: TextStyle(fontSize: 16, color: KGrey2),),
+                                SizedBox(width: 95,)
+                              ],
+                            ),
+                            SizedBox(height: 2,),
+                            button(
+                                iconRightFlex: 1,
+                                iconLeftFlex: 1,
+                                textFlex: 2,
+                                contentFlex: 0,
+                                text: '16:00',
+                                fontSize: 16,
+                                iconLeft: Icons.access_time,
+                                iconRight: Icons.keyboard_arrow_down,
+                                iconLeftSize: 22,
+                                iconRightSize: 22,
+                                function: (){
+                                  setState(() {
+                                    isFromVisible = !isFromVisible;
+                                  });
+                                },
+                                height: 40,
+                                width: 140,
+                                content: Container()
+                            ),
+                          ],
+                        ),
+                        SizedBox(width: 20,),
+                        Column(
+                          children: [
+                            Row(
+                              children: [
+                                Text('Hasta:', style: TextStyle(fontSize: 16, color: KGrey2),),
+                                SizedBox(width: 100,)
+                              ],
+                            ),
+                            SizedBox(height: 2,),
+                            button(
+                                iconRightFlex: 1,
+                                iconLeftFlex: 1,
+                                textFlex: 2,
+                                contentFlex: 0,
+                                text: '20:00',
+                                fontSize: 16,
+                                iconLeft: Icons.access_time,
+                                iconRight: Icons.keyboard_arrow_down,
+                                iconLeftSize: 22,
+                                iconRightSize: 22,
+                                function: (){
+                                  setState(() {
+                                    isToVisible = !isToVisible;
+                                  });
+                                },
+                                height: 40,
+                                width: 140,
+                                content: Container()
+                            ),
+                          ],
+                        ),
 
-                    ],
-                  )
-              ),
-              //eventDayFromAndTo(50, isEventDayVisible, KRed, context),
-              Positioned(
-                  top: 5,
-                  left: 10,
-                  child: Column(
-                    children: [
-                      Row(
-                        children: [
-                          Text('Elija día del evento:', style: TextStyle(fontSize: 20, color: KGrey2),),
-                          SizedBox(width: 130,)
-                        ],
-                      ),
-                      SizedBox(height: 5,),
-                      button(
-                          iconLeftFlex: 1,
-                          iconRightFlex: 1,
-                          textFlex: 6,
-                          contentFlex: 3,
-                          text: ' 09/08/2023',
-                          fontSize: 16,
-                          iconLeft: Icons.calendar_today,
-                          iconRight: Icons.keyboard_arrow_down,
-                              function: (){
-                            setState(() {
-                              isEventDayVisible = !isEventDayVisible;
-                            });
-                          },
-                        height: 40,
-                        width: 300,
-                        content: Container()
-                      ),
-                    ],
-                  )
-              ),
-              isEventDayVisible? Positioned(
-                top: 90,
-                left: 10,
-                child: Container(
-                  width: 300,
-                  height: 400,
-                  decoration: const BoxDecoration(
-                      color: KGrey5,
-                      boxShadow: [
-                        BoxShadow(
-                            color: KGray,
-                            blurRadius: 12,
-                            offset: Offset(0, 4))]
-                  ),
-                  child: CalendarDatePicker(
-                      initialDate: initialDate,
-                      firstDate: initialDate,
-                      lastDate: initialDate,
-                      onDateChanged: (date){}
-                  ),
-                ),
-              ) : Container(),
+                      ],
+                    ),
 
-              Positioned(
-                child: Container(),
-              )
-            ],
+                  ],
+                ),
+
+                isToVisible? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        SizedBox(height: 150,),
+                        Container(
+                          width: 300,
+                          height: 400,
+                          color: KGray,
+                          child: Center(
+                            child: Text('Widget Superpuesto'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ) : Container(),
+
+                isFromVisible? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        SizedBox(height: 150,),
+                        Container(
+                          width: 300,
+                          height: 400,
+                          color: KRed,
+                          child: Center(
+                            child: Text('Widget Superpuesto'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ) : Container(),
+
+                isEventDayVisible? Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        SizedBox(height: 80,),
+                        Container(
+                          width: 300,
+                          height: 400,
+                          decoration: const BoxDecoration(
+                              color: KGrey5,
+                              boxShadow: [
+                                BoxShadow(
+                                    color: KGray,
+                                    blurRadius: 12,
+                                    offset: Offset(0, 4))]
+                          ),
+                          child: CalendarDatePicker(
+                              initialDate: initialDate,
+                              firstDate: initialDate,
+                              lastDate: initialDate,
+                              onDateChanged: (date){}
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ) : Container(),
+
+                /*Positioned(
+                  child: Container(),
+                )*/
+              ],
+            ),
           ),
         ),
       ),
@@ -415,6 +440,8 @@ class _ChooseDayAndTimeScreenState extends StateMVC<ChooseDayAndTimeScreen> {
   Widget button({
     required String text,
     required int iconLeftFlex,
+    double? iconLeftSize,
+    double? iconRightSize,
     required int iconRightFlex,
     required int textFlex,
     required int contentFlex,
@@ -435,10 +462,10 @@ class _ChooseDayAndTimeScreenState extends StateMVC<ChooseDayAndTimeScreen> {
           padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 5),
           child: Row(
             children: [
-              Expanded(flex: iconLeftFlex, child: Icon(iconLeft, color: KGrey5,)),
+              Expanded(flex: iconLeftFlex, child: Icon(iconLeft, color: KGrey5, size: iconLeftSize,)),
               Expanded(flex: textFlex, child: Text(text, style: TextStyle(fontSize: fontSize, color: KGrey5),)),
               Expanded(flex: contentFlex, child: content),
-              Expanded(flex: iconRightFlex, child: Icon(iconRight, color: KGrey5,)),
+              Expanded(flex: iconRightFlex, child: Icon(iconRight, color: KGrey5, size: iconRightSize,)),
             ],
           ),
         ),
